62260ff82df18162c43f58be1aa3f3ea29badf44
Gianna Drego <giannadrego1503@gmail.com>
2020-08-11 15:24:21 +0530

    cookie

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(session({
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
     cookie: {
-
+        sameSite: 'none'
     }
     // cookie: {
     //     maxAge: 1000 * 60 * 60 * 24
